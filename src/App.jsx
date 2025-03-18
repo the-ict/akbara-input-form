8dd4dac400cdb41908db62d9396a2b4deb324e40
dvlt-org <dvltinv@gmail.com>
2025-03-18 12:33:57 +0300

    initial-commit

---
+++ src/App.jsx
@@ -0,0 +1,208 @@
+import React, { useEffect, useState } from 'react'
+import { countries, regions } from "./country"
+import Logo from "../public/akbaraImg.jpg"
+import "./index.css"
+
+
+export default function App() {
+  const [name, setName] = useState("")
+  const [lastName, setLastName] = useState("")
+  const [phone, setPhone] = useState(0)
+  const [country, setCountry] = useState("Uzbekistan")
+  const [region, setRegion] = useState("Andijon viloyati")
+  const [districts, setDistricts] = useState("")
+
+  // Tilga moslashtirilgan matnlar
+  const [labels, setLabels] = useState({});
+
+  useEffect(() => {
+    const languageCode = window.Telegram.WebApp.initDataUnsafe.user?.language_code;
+
+    if (languageCode === "uz") {
+      setLabels({
+        nameLabel: "Ism",
+        lastNameLabel: "Familiya",
+        phoneLabel: "Telefon",
+        countryLabel: "Davlat",
+        regionLabel: "Viloyat",
+        districtLabel: "Tuman",
+        submitButton: "Keyingisi",
+        cancelButton: "Bekor qilish",
+        namePlaceholder: "Ismni kiriting!",
+        lastNamePlaceholder: "Familiyani kiriting",
+        phonePlaceholder: "Telefon raqamingiz",
+      });
+    } else if (languageCode === "en") {
+      setLabels({
+        nameLabel: "First Name",
+        lastNameLabel: "Last Name",
+        phoneLabel: "Phone",
+        countryLabel: "Country",
+        regionLabel: "Region",
+        districtLabel: "District",
+        submitButton: "Next",
+        cancelButton: "Cancel",
+        namePlaceholder: "Enter your first name!",
+        lastNamePlaceholder: "Enter your last name",
+        phonePlaceholder: "Enter your phone number",
+      });
+    } else if (languageCode === "ru") {
+      setLabels({
+        nameLabel: "Имя",
+        lastNameLabel: "Фамилия",
+        phoneLabel: "Телефон",
+        countryLabel: "Страна",
+        regionLabel: "Регион",
+        districtLabel: "Район",
+        submitButton: "Далее",
+        cancelButton: "Отменить",
+        namePlaceholder: "Введите ваше имя!",
+        lastNamePlaceholder: "Введите вашу фамилию",
+        phonePlaceholder: "Введите ваш номер телефона",
+      });
+    } else {
+      setLabels({
+        nameLabel: "Ism",
+        lastNameLabel: "Familiya",
+        phoneLabel: "Telefon",
+        countryLabel: "Davlat",
+        regionLabel: "Viloyat",
+        districtLabel: "Tuman",
+        submitButton: "Keyingisi",
+        cancelButton: "Bekor qilish",
+        namePlaceholder: "Ismni kiriting!",
+        lastNamePlaceholder: "Familiyani kiriting",
+        phonePlaceholder: "Telefon raqamingiz",
+      });
+    }
+    window.Telegram.WebApp.ready()
+  }, []);
+
+  const handleSubmit = () => {
+    if (!name || !lastName || !phone || !country) {
+      alert("Iltimos, barcha maydonlarni to'ldiring.");
+      return;
+    }
+
+    try {
+      if (!window.Telegram || !window.Telegram.WebApp) {
+        alert("Telegram WebApp yuklanmagan!");
+        return;
+      }
+
+      alert(window?.Telegram?.WebApp)
+
+      if (window?.Telegram?.WebApp.sendData) {
+        const data = {
+          name,
+          lastName,
+          country,
+          region,
+          districts,
+          phone: String(phone),
+        };
+
+        window?.Telegram?.WebApp.sendData(JSON.stringify(data));
+      } else {
+        alert("sendData funcksiyasi ishlamayapti!")
+      }
+
+    } catch (error) {
+      alert("Xatolik yuz berdi: " + error);
+    }
+  };
+
+
+  const handleCancel = () => {
+    window.Telegram.WebApp.close()
+  }
+
+  return (
+    <div className='app'>
+      <form className='app-form' onSubmit={(e) => {
+        e.preventDefault()
+      }} style={{
+        backgroundColor: window.Telegram.WebApp.themeParams.bg_color && window.Telegram.WebApp.themeParams.bg_color,
+        color: window.Telegram.WebApp.themeParams.text_color && window.Telegram.WebApp.themeParams.text_color
+      }}>
+        <div className='app-form__content'>
+          <a href="/">
+            <img src={Logo} alt="akbaraTv.logo" />
+          </a>
+          <div>
+            <label>{labels.nameLabel}</label>
+            <input type="text" placeholder={labels.namePlaceholder} onChange={(e) => {
+              setName(e.target.value)
+            }} />
+          </div>
+          <div>
+            <label>{labels.lastNameLabel}</label>
+            <input type="text" placeholder={labels.lastNamePlaceholder} onChange={(e) => {
+              setLastName(e.target.value)
+            }} />
+          </div>
+          <div>
+            <label>{labels.phoneLabel}</label>
+            <input type="number" placeholder={labels.phonePlaceholder} onChange={(e) => {
+              setPhone(Number(e.target.value))
+            }} />
+          </div>
+          <div>
+            <label>{labels.countryLabel}</label>
+            <select onChange={(e) => {
+              setCountry(e.target.value)
+            }} defaultValue="Uzbekistan">
+              {
+                countries.map((item, index) => {
+                  return (
+                    <option value={item} key={index}>{item}</option>
+                  )
+                })
+              }
+            </select>
+          </div>
+          {
+            country === "Uzbekistan" && (
+              <>
+                <div>
+                  <label>{labels.regionLabel}</label>
+                  <select onChange={(e) => {
+                    setRegion(e.target.value)
+                  }} defaultValue="Andijon viloyati">
+                    {
+                      regions.map((item, index) => (
+                        <option value={item.region} key={index}>{item.region}</option>
+                      ))
+                    }
+                  </select>
+                </div>
+                <div>
+                  <label>{labels.districtLabel}</label>
+                  <select onChange={(e) => {
+                    setDistricts(e.target.value)
+                  }}>
+                    {
+                      regions.map(item => {
+                        if (item.region === region) {
+                          return (
+                            item.districts.map((dis, _i) => (
+                              <option value={dis} key={_i}>{dis}</option>
+                            ))
+                          )
+                        }
+                      })
+                    }
+                  </select>
+                </div>
+              </>
+            )
+          }
+          <div className='app-form__buttons'>
+            <button onClick={handleCancel}>{labels.cancelButton}</button>
+            <button onClick={handleSubmit}>{labels.submitButton}</button>
+          </div>
+        </div>
+      </form>
+    </div>
+  )
+}
---
+++ src/country.js
@@ -0,0 +1,188 @@
+export const countries = [
+    "Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Antigua and Barbuda", "Argentina", "Armenia", "Australia", "Austria",
+    "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bhutan", "Bolivia",
+    "Bosnia and Herzegovina", "Botswana", "Brazil", "Brunei", "Bulgaria", "Burkina Faso", "Burundi", "Cabo Verde", "Cambodia",
+    "Cameroon", "Canada", "Central African Republic", "Chad", "Chile", "China", "Colombia", "Comoros", "Congo", "Congo (Democratic Republic)",
+    "Costa Rica", "Croatia", "Cuba", "Cyprus", "Czech Republic", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "Ecuador",
+    "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", "Eswatini", "Ethiopia", "Fiji", "Finland", "France", "Gabon",
+    "Gambia", "Georgia", "Germany", "Ghana", "Greece", "Grenada", "Guatemala", "Guinea", "Guinea-Bissau", "Guyana", "Haiti", "Honduras",
+    "Hungary", "Iceland", "India", "Indonesia", "Iran", "Iraq", "Ireland", "Israel", "Italy", "Jamaica", "Japan", "Jordan", "Kazakhstan",
+    "Kenya", "Kiribati", "Korea (North)", "Korea (South)", "Kuwait", "Kyrgyzstan", "Laos", "Latvia", "Lebanon", "Lesotho", "Liberia",
+    "Libya", "Liechtenstein", "Lithuania", "Luxembourg", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands",
+    "Mauritania", "Mauritius", "Mexico", "Micronesia", "Moldova", "Monaco", "Mongolia", "Montenegro", "Morocco", "Mozambique", "Myanmar",
+    "Namibia", "Nauru", "Nepal", "Netherlands", "New Zealand", "Nicaragua", "Niger", "Nigeria", "North Macedonia", "Norway", "Oman",
+    "Pakistan", "Palau", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Poland", "Portugal", "Qatar", "Romania",
+    "Russia", "Rwanda", "Saint Kitts and Nevis", "Saint Lucia", "Saint Vincent and the Grenadines", "Samoa", "San Marino", "Sao Tome and Principe",
+    "Saudi Arabia", "Senegal", "Serbia", "Seychelles", "Sierra Leone", "Singapore", "Slovakia", "Slovenia", "Solomon Islands", "Somalia",
+    "South Africa", "South Sudan", "Spain", "Sri Lanka", "Sudan", "Suriname", "Sweden", "Switzerland", "Syria", "Taiwan", "Tajikistan",
+    "Tanzania", "Thailand", "Timor-Leste", "Togo", "Tonga", "Trinidad and Tobago", "Tunisia", "Turkey", "Turkmenistan", "Tuvalu",
+    "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "United States", "Uruguay", "Uzbekistan", "Vanuatu", "Vatican City",
+    "Venezuela", "Vietnam", "Yemen", "Zambia", "Zimbabwe"
+];
+
+
+
+
+export const regions = [
+    {
+        region: "Andijon viloyati",
+        districts: [
+            "Andijon",
+            "Asaka",
+            "Baliqchi",
+            "Buloqboshi",
+            "Davlatabad",
+            "Jalaquduq",
+            "Khodjaobod",
+            "Qo'rg'ontepa",
+            "Marhamat",
+            "Paxtaobod",
+            "Xonobod"
+        ]
+    },
+    {
+        region: "Buxoro viloyati",
+        districts: [
+            "Buxoro",
+            "G'ijduvon",
+            "Kogon",
+            "Jondor",
+            "Olot",
+            "Peshku",
+            "Romitan",
+            "Shofirkon",
+            "Vobkent"
+        ]
+    },
+    {
+        region: "Farg'ona viloyati",
+        districts: [
+            "Farg'ona",
+            "Bag'dod",
+            "Buvayda",
+            "Dang'ara",
+            "Farg'ona",
+            "Koshtepa",
+            "Oltiariq",
+            "Quva",
+            "Rishton",
+            "Tashlak",
+            "Uchko'prik"
+        ]
+    },
+    {
+        region: "Jizzax viloyati",
+        districts: [
+            "Jizzax",
+            "Arnasoy",
+            "Bakhmal",
+            "Dustlik",
+            "G'allaorol",
+            "Forish",
+            "Zafarobod",
+            "Sharof Rashidov"
+        ]
+    },
+    {
+        region: "Qashqadaryo viloyati",
+        districts: [
+            "Qarshi",
+            "Chiroqchi",
+            "Dehqonobod",
+            "Koson",
+            "Kitob",
+            "Mirishkor",
+            "Muborak",
+            "Nishon",
+            "Shahrisabz",
+            "Yakkabog'"
+        ]
+    },
+    {
+        region: "Navoiy viloyati",
+        districts: [
+            "Navoiy",
+            "Karmana",
+            "Konimex",
+            "Nurota",
+            "Tomdi",
+            "Zafarobod"
+        ]
+    },
+    {
+        region: "Namangan viloyati",
+        districts: [
+            "Namangan",
+            "Chortoq",
+            "Kosonsoy",
+            "Mingbuloq",
+            "Namangan",
+            "Pop",
+            "Uychi"
+        ]
+    },
+    {
+        region: "Samarqand viloyati",
+        districts: [
+            "Samarqand",
+            "Bulung'ur",
+            "Jomboy",
+            "Kattaqo'rg'on",
+            "Payariq",
+            "Paxtachi",
+            "Samarkand",
+            "Urgut"
+        ]
+    },
+    {
+        region: "Sirdaryo viloyati",
+        districts: [
+            "Guliston",
+            "Baxt",
+            "Davlatabad",
+            "Mirzaobod",
+            "Sayxunabad",
+            "Sirdaryo"
+        ]
+    },
+    {
+        region: "Surxondaryo viloyati",
+        districts: [
+            "Termiz",
+            "Angor",
+            "Denov",
+            "Boysun",
+            "Jarqo'rg'on",
+            "Oltinsoy",
+            "Shurchi",
+            "Muzrabot"
+        ]
+    },
+    {
+        region: "Toshkent viloyati",
+        districts: [
+            "Toshkent",
+            "Bekabad",
+            "Zangiota",
+            "Qibray",
+            "Chirchiq",
+            "Parkent",
+            "Yangiyo'l",
+            "Buka",
+            "Ohangaron"
+        ]
+    },
+    {
+        region: "Xorazm viloyati",
+        districts: [
+            "Urganch",
+            "Beruniy",
+            "Xiva",
+            "Shovot",
+            "Hazorasp",
+            "Qoshkupir",
+            "Yangibozor",
+            "Urgench"
+        ]
+    }
+];
